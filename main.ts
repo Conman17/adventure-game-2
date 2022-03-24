f2f971f2014e5a6a81039fbab36227cd090ebd43
Conman17 <101826224+Conman17@users.noreply.github.com>
2022-03-23 20:36:57 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,315 @@
+input.onButtonPressed(Button.A, function () {
+    Character.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.AB, function () {
+    Character.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    Character.change(LedSpriteProperty.X, 1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Character.change(LedSpriteProperty.Y, -1)
+})
+let Pain = 0
+let Keys = 0
+let Key_3_last = 0
+let Egg = 0
+let Room = 0
+let score_for_keys = 0
+let Character: game.LedSprite = null
+Character = game.createSprite(2, 2)
+let Key2 = game.createSprite(0, 0)
+let Key_3 = game.createSprite(0, 0)
+let Key_1 = game.createSprite(0, 0)
+basic.forever(function () {
+    if (score_for_keys == 3) {
+        basic.showString("YOU WIN")
+        game.gameOver()
+    }
+    if (Room == 0) {
+        led.plot(0, 0)
+        led.plot(1, 0)
+        led.plot(3, 0)
+        led.plot(4, 0)
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.plot(1, 4)
+        led.plot(3, 4)
+        led.plot(4, 4)
+        led.plot(4, 3)
+        led.plot(4, 2)
+        led.plot(4, 1)
+        if (Egg == 1) {
+            basic.clearScreen()
+            basic.pause(500)
+            basic.showString("TRUE WINNER")
+            game.setScore(100)
+            game.gameOver()
+        }
+        if (Character.get(LedSpriteProperty.X) == 2) {
+            if (Character.get(LedSpriteProperty.Y) == 4) {
+                Room = 66
+                Character.set(LedSpriteProperty.Y, 1)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 0) {
+            Character.set(LedSpriteProperty.X, 1)
+        }
+        if (Character.get(LedSpriteProperty.X) == 4) {
+            Character.set(LedSpriteProperty.X, 3)
+        }
+        if (Character.get(LedSpriteProperty.Y) == 4) {
+            if (Character.get(LedSpriteProperty.X) == 1) {
+                Character.set(LedSpriteProperty.Y, 3)
+            }
+        }
+        if (Character.get(LedSpriteProperty.Y) == 4) {
+            if (Character.get(LedSpriteProperty.X) == 3) {
+                Character.set(LedSpriteProperty.Y, 3)
+            }
+        }
+        if (Character.get(LedSpriteProperty.Y) == 0) {
+            if (Character.get(LedSpriteProperty.X) == 3) {
+                Character.set(LedSpriteProperty.Y, 1)
+            }
+        }
+        if (Character.get(LedSpriteProperty.Y) == 0) {
+            if (Character.get(LedSpriteProperty.X) == 1) {
+                Character.set(LedSpriteProperty.Y, 1)
+            }
+        }
+        if (Character.get(LedSpriteProperty.Y) == 0) {
+            if (Character.get(LedSpriteProperty.X) == 2) {
+                Character.set(LedSpriteProperty.Y, 3)
+                Room = 1
+            }
+        }
+    }
+    if (Room == 1) {
+        led.plot(1, 0)
+        led.plot(1, 1)
+        led.plot(1, 2)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.plot(3, 0)
+        led.plot(3, 1)
+        led.plot(3, 2)
+        led.plot(3, 3)
+        led.plot(3, 4)
+        Key_1.set(LedSpriteProperty.X, 1)
+        Key_1.set(LedSpriteProperty.Y, 0)
+        Key2.set(LedSpriteProperty.X, 1)
+        Key2.set(LedSpriteProperty.Y, 0)
+        Key_3.set(LedSpriteProperty.X, 1)
+        Key_3.set(LedSpriteProperty.Y, 0)
+        if (Character.get(LedSpriteProperty.X) == 1) {
+            Character.set(LedSpriteProperty.X, 2)
+        }
+        if (Character.get(LedSpriteProperty.X) == 3) {
+            Character.set(LedSpriteProperty.X, 2)
+        }
+        if (Character.get(LedSpriteProperty.Y) == 4) {
+            Room = 0
+            Character.set(LedSpriteProperty.Y, 1)
+        }
+        if (Character.get(LedSpriteProperty.Y) == 0) {
+            Room = 2
+            Character.set(LedSpriteProperty.Y, 3)
+        }
+    }
+    if (Room == 2) {
+        led.plot(1, 0)
+        led.plot(1, 1)
+        led.plot(0, 1)
+        led.plot(3, 0)
+        led.plot(3, 1)
+        led.plot(4, 1)
+        led.plot(0, 3)
+        led.plot(1, 3)
+        led.plot(1, 4)
+        led.plot(3, 3)
+        led.plot(3, 4)
+        led.plot(4, 3)
+        Key_1.set(LedSpriteProperty.X, 1)
+        Key_1.set(LedSpriteProperty.Y, 0)
+        Key2.set(LedSpriteProperty.X, 1)
+        Key2.set(LedSpriteProperty.Y, 0)
+        if (Key_3.get(LedSpriteProperty.X) == Character.get(LedSpriteProperty.X)) {
+            if (Key_3.get(LedSpriteProperty.Y) == Character.get(LedSpriteProperty.Y)) {
+                Key_3.set(LedSpriteProperty.X, 1)
+                Key_3.set(LedSpriteProperty.Y, 0)
+                basic.pause(100)
+                game.addScore(1)
+                score_for_keys += 1
+            }
+        }
+        if (Key_3_last == 0) {
+            Key_3.set(LedSpriteProperty.X, 3)
+            Key_3.set(LedSpriteProperty.Y, 2)
+            Key_3_last = 1
+        }
+        if (Character.get(LedSpriteProperty.X) == 0) {
+            if (Character.get(LedSpriteProperty.Y) == 2) {
+                Room = 3
+                Character.set(LedSpriteProperty.X, 3)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 2) {
+            if (Character.get(LedSpriteProperty.Y) == 0) {
+                Character.set(LedSpriteProperty.Y, 3)
+                Character.set(LedSpriteProperty.X, 2)
+                basic.pause(100)
+                Room = 0
+            }
+        }
+        if (score_for_keys == 2) {
+            if (Character.get(LedSpriteProperty.X) == 0) {
+                if (Character.get(LedSpriteProperty.Y) == 0) {
+                    if (Egg == 0) {
+                        Egg = 1
+                    }
+                }
+            }
+        } else {
+            if (Character.get(LedSpriteProperty.X) == 1) {
+                if (Character.get(LedSpriteProperty.Y) == 1) {
+                    Character.set(LedSpriteProperty.X, 2)
+                    Character.set(LedSpriteProperty.Y, 2)
+                }
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 3) {
+            if (Character.get(LedSpriteProperty.Y) == 1) {
+                Character.set(LedSpriteProperty.X, 2)
+                Character.set(LedSpriteProperty.Y, 2)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 3) {
+            if (Character.get(LedSpriteProperty.Y) == 3) {
+                Character.set(LedSpriteProperty.X, 2)
+                Character.set(LedSpriteProperty.Y, 2)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 1) {
+            if (Character.get(LedSpriteProperty.Y) == 3) {
+                Character.set(LedSpriteProperty.X, 2)
+                Character.set(LedSpriteProperty.Y, 2)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 4) {
+            Room = 3
+            Character.set(LedSpriteProperty.X, 1)
+        }
+        if (Character.get(LedSpriteProperty.Y) == 4) {
+            Room = 1
+            Character.set(LedSpriteProperty.Y, 1)
+        }
+    }
+    if (Room == 3) {
+        led.plot(0, 1)
+        led.plot(1, 1)
+        led.plot(2, 1)
+        led.plot(3, 1)
+        led.plot(4, 1)
+        led.plot(0, 3)
+        led.plot(1, 3)
+        led.plot(2, 3)
+        led.plot(3, 3)
+        led.plot(4, 3)
+        Key_1.set(LedSpriteProperty.X, 0)
+        Key_1.set(LedSpriteProperty.Y, 3)
+        Key_3.set(LedSpriteProperty.X, 0)
+        Key_3.set(LedSpriteProperty.Y, 3)
+        if (Key2.get(LedSpriteProperty.X) == Character.get(LedSpriteProperty.X)) {
+            if (Key2.get(LedSpriteProperty.Y) == Character.get(LedSpriteProperty.Y)) {
+                Key2.set(LedSpriteProperty.X, 0)
+                Key2.set(LedSpriteProperty.Y, 3)
+                basic.pause(100)
+                game.addScore(1)
+                score_for_keys += 1
+            }
+        }
+        if (Keys == 0) {
+            Key2.set(LedSpriteProperty.X, 2)
+            Key2.set(LedSpriteProperty.Y, 2)
+            Keys = 1
+        }
+        if (Character.get(LedSpriteProperty.Y) == 1) {
+            Character.set(LedSpriteProperty.Y, 2)
+        }
+        if (Character.get(LedSpriteProperty.Y) == 3) {
+            Character.set(LedSpriteProperty.Y, 2)
+        }
+        if (Character.get(LedSpriteProperty.X) == 4) {
+            if (Character.get(LedSpriteProperty.Y) == 2) {
+                Room = 2
+                Character.set(LedSpriteProperty.X, 1)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 0) {
+            if (Character.get(LedSpriteProperty.Y) == 2) {
+                Room = 2
+                Character.set(LedSpriteProperty.X, 3)
+            }
+        }
+    }
+    if (Room == 66) {
+        led.plot(0, 0)
+        led.plot(0, 1)
+        led.plot(0, 2)
+        led.plot(0, 3)
+        led.plot(0, 4)
+        led.plot(1, 0)
+        led.plot(3, 0)
+        led.plot(4, 0)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
+        led.plot(4, 4)
+        Key_3.set(LedSpriteProperty.X, 0)
+        Key_3.set(LedSpriteProperty.Y, 0)
+        Key2.set(LedSpriteProperty.X, 0)
+        Key2.set(LedSpriteProperty.Y, 0)
+        if (Key_1.get(LedSpriteProperty.X) == Character.get(LedSpriteProperty.X)) {
+            if (Key_1.get(LedSpriteProperty.Y) == Character.get(LedSpriteProperty.Y)) {
+                Key_1.set(LedSpriteProperty.X, 0)
+                Key_1.set(LedSpriteProperty.Y, 0)
+                basic.pause(100)
+                score_for_keys = 1
+                game.addScore(1)
+            }
+        }
+        if (Pain == 0) {
+            Key_1.set(LedSpriteProperty.X, randint(1, 3))
+            Key_1.set(LedSpriteProperty.Y, randint(1, 3))
+            Pain = 1
+        }
+        if (Character.get(LedSpriteProperty.X) == 2) {
+            if (Character.get(LedSpriteProperty.Y) == 0) {
+                Room = 0
+                Character.set(LedSpriteProperty.Y, 3)
+            }
+        }
+        if (Character.get(LedSpriteProperty.X) == 0) {
+            Character.set(LedSpriteProperty.X, 1)
+        }
+        if (Character.get(LedSpriteProperty.X) == 4) {
+            Character.set(LedSpriteProperty.X, 3)
+        }
+        if (Character.get(LedSpriteProperty.Y) == 0) {
+            if (Character.get(LedSpriteProperty.X) == 1) {
+                Character.set(LedSpriteProperty.Y, 1)
+            }
+            if (Character.get(LedSpriteProperty.X) == 3) {
+                Character.set(LedSpriteProperty.Y, 1)
+            }
+        }
+        if (Character.get(LedSpriteProperty.Y) == 4) {
+            Room = 3
+            basic.pause(100)
+            Character.set(LedSpriteProperty.Y, 0)
+        }
+    }
+})
